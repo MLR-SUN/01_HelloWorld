81fef36090c334a16430c5521059640c56e8208e
M.L.R <sun.ios06444@gmail.com>
2018-01-16 22:12:23 +0800

    調整label顯示出來的顏色

--- 01_HelloWord/ViewController.swift
+++ 01_HelloWord/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     @IBAction func onClick(_ sender: Any) {
         label.text = "HelloWorld"
+        label.textColor = UIColor.red
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
